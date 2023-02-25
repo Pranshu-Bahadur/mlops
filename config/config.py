a503589381d7840b5adcdf735e6cc2ab357f3dbd
verus-carver <pranshubahadur@gmail.com>
2023-02-25 23:39:47 +0530

    So rusty.

--- config/config.py
+++ config/config.py
@@ -1,17 +1,12 @@
-# config/config.py
 from pathlib import Path
 import pretty_errors
 
-# Directories
 BASE_DIR = Path(__file__).parent.parent.absolute()
 CONFIG_DIR = Path(BASE_DIR, "config")
 DATA_DIR = Path(BASE_DIR, "data")
 
-# Create dirs
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-
-# Assets
 PROJECTS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/projects.csv"
 TAGS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/tags.csv"
 
